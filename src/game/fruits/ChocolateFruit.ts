0cc9c19e4664833b35e36432d11ab8304e875558
HenryLimaBR <djfilipebr@gmail.com>
2022-05-10 11:54:22 -0300

    Deploy Fix

--- src/game/fruits/ChocolateFruit.ts
+++ src/game/fruits/ChocolateFruit.ts
@@ -1,11 +1,11 @@
 import { fruitHeartAnimation } from '../animations/fruits/heart-fruit-animation'
-import { GameFruit } from '../fruit'
+import { Fruit } from '../fruit'
 
 type HeartFruitConstructorOptions = {
   x: number, y: number
 }
 
-export class HeartFruit extends GameFruit {
+export class HeartFruit extends Fruit {
   constructor({ x, y }: HeartFruitConstructorOptions) {
     super({ x, y, ad: fruitHeartAnimation })
   }
